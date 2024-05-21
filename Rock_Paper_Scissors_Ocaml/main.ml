efb228bb960fe9b217f73350c5d98f8926b4e358
Nilay Ghosh <nilayghosh66@gmail.com>
2024-05-22 00:21:26 +0530

    Ocaml code fix

--- Rock_Paper_Scissors_Ocaml/main.ml
+++ Rock_Paper_Scissors_Ocaml/main.ml
@@ -1,4 +1,9 @@
 let rec play_game() =
   print_endline "Enter your choice (rock/paper/scissors): "; 
-  let user_choice = read_line () in
-  String.lowercase_ascii user_choice
+  let user_choice = read_line () |> String.lowercase_ascii in
+  while user_choice <> rock || user_choice <> paper || user_choice <> scissors
+  print_endline "Invalid choice! Please enter rock, paper, or scissors."
+  play_game()
+;;
+
+play_game();
